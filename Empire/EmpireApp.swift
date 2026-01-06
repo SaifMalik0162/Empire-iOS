a8c752f5333faca073800976a03a403db31328b2
SaifMalik0162 <saifmmalik08@gmail.com>
2026-01-06 15:39:28 -0500

    Initial Commit

---
+++ Empire/EmpireApp.swift
@@ -0,0 +1,32 @@
+//
+//  EmpireApp.swift
+//  Empire
+//
+//  Created by Saif Malik on 2026-01-06.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EmpireApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ EmpireTests/EmpireTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmpireTests.swift
+//  EmpireTests
+//
+//  Created by Saif Malik on 2026-01-06.
+//
+
+import Testing
+@testable import Empire
+
+struct EmpireTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
