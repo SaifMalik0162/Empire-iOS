93a4c92e187010d70797f2403cc4ffea2134bc4f
SaifMalik0162 <saifmmalik08@gmail.com>
2026-01-10 15:43:03 -0500

    bug: Fixed app crash when opening cart

--- Empire/App/EmpireApp.swift
+++ Empire/App/EmpireApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct EmpireApp: App {
+    @StateObject private var cart = Cart()
+    
     var body: some Scene {
         WindowGroup {
             LoginView()
+                .environmentObject(cart)
                 .preferredColorScheme(.dark)
         }
     }
